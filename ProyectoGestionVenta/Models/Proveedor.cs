5f3f8c5b8aa78da663a189348ed35d5b0aaa2cf0
Ismarlyn Mendoza Hernández <Marlynm123@hotmail.com>
2023-09-19 23:12:52 -0400

    Mensajes de errores en los campos de Proveedor implementados

--- ProyectoGestionVenta/Models/Proveedor.cs
+++ ProyectoGestionVenta/Models/Proveedor.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoGestionVenta.Models
 {
@@ -11,11 +13,20 @@ namespace ProyectoGestionVenta.Models
         }
 
         public int ProveedorId { get; set; }
+        [Required(ErrorMessage = "Favor ingresar correctamente el Rnc.")]
         public int? Rnc { get; set; }
+        [Required (ErrorMessage = "Favor ingresar correctamente el Nombre.")]
         public string? Nombre { get; set; }
+        [Remote(action: "VerifyEmail", controller: "Users")]
+        [EmailAddress(ErrorMessage = "Ingrese correctamente el Correo.")]
         public string Correo { get; set; } = null!;
+        [Required(ErrorMessage = "Favor ingresar correctamente el Representante.")]
         public string Representante { get; set; } = null!;
+        [Required(ErrorMessage = "Favor ingresar correctamente su Dirección.")]
         public string Direccion { get; set; } = null!;
+
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}$")]
+        [Phone(ErrorMessage = "Favor de ingresar correctamente el numero de Contacto.")]
         public string Contacto { get; set; } = null!;
 
         public virtual ICollection<Articulo> Articulos { get; set; }
