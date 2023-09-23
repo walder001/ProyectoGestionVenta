278185652428e79b13f7822d67fc82b0222d8556
Edisonjesus <klkjesus.con@gmail.com>
2023-09-23 10:35:50 -0400

    se agreguo  un nuebo controlador

---
+++ ProyectoGestionVenta/Controllers/ArticuloesController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProyectoGestionVenta.Models;
+
+namespace ProyectoGestionVenta.Controllers
+{
+    public class ArticuloesController : Controller
+    {
+        private readonly GestionVentasContext _context;
+
+        public ArticuloesController(GestionVentasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Articuloes
+        public async Task<IActionResult> Index()
+        {
+            var gestionVentasContext = _context.Articulos.Include(a => a.Categoria).Include(a => a.Proveedor);
+            return View(await gestionVentasContext.ToListAsync());
+        }
+
+        // GET: Articuloes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Articulos == null)
+            {
+                return NotFound();
+            }
+
+            var articulo = await _context.Articulos
+                .Include(a => a.Categoria)
+                .Include(a => a.Proveedor)
+                .FirstOrDefaultAsync(m => m.ArticuloId == id);
+            if (articulo == null)
+            {
+                return NotFound();
+            }
+
+            return View(articulo);
+        }
+
+        // GET: Articuloes/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoriaId"] = new SelectList(_context.Categoria, "CategoriaId", "CategoriaId");
+            ViewData["ProveedorId"] = new SelectList(_context.Proveedors, "ProveedorId", "Direccion");
+            return View();
+        }
+
+        // POST: Articuloes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ArticuloId,CategoriaId,ProveedorId,Codigo,Nombre,PrecioVenta,Stock,Descripcion,Estado")] Articulo articulo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(articulo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categoria, "CategoriaId", "CategoriaId", articulo.CategoriaId);
+            ViewData["ProveedorId"] = new SelectList(_context.Proveedors, "ProveedorId", "Direccion", articulo.ProveedorId);
+            return View(articulo);
+        }
+
+        // GET: Articuloes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Articulos == null)
+            {
+                return NotFound();
+            }
+
+            var articulo = await _context.Articulos.FindAsync(id);
+            if (articulo == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categoria, "CategoriaId", "CategoriaId", articulo.CategoriaId);
+            ViewData["ProveedorId"] = new SelectList(_context.Proveedors, "ProveedorId", "Direccion", articulo.ProveedorId);
+            return View(articulo);
+        }
+
+        // POST: Articuloes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ArticuloId,CategoriaId,ProveedorId,Codigo,Nombre,PrecioVenta,Stock,Descripcion,Estado")] Articulo articulo)
+        {
+            if (id != articulo.ArticuloId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(articulo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArticuloExists(articulo.ArticuloId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categoria, "CategoriaId", "CategoriaId", articulo.CategoriaId);
+            ViewData["ProveedorId"] = new SelectList(_context.Proveedors, "ProveedorId", "Direccion", articulo.ProveedorId);
+            return View(articulo);
+        }
+
+        // GET: Articuloes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Articulos == null)
+            {
+                return NotFound();
+            }
+
+            var articulo = await _context.Articulos
+                .Include(a => a.Categoria)
+                .Include(a => a.Proveedor)
+                .FirstOrDefaultAsync(m => m.ArticuloId == id);
+            if (articulo == null)
+            {
+                return NotFound();
+            }
+
+            return View(articulo);
+        }
+
+        // POST: Articuloes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Articulos == null)
+            {
+                return Problem("Entity set 'GestionVentasContext.Articulos'  is null.");
+            }
+            var articulo = await _context.Articulos.FindAsync(id);
+            if (articulo != null)
+            {
+                _context.Articulos.Remove(articulo);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArticuloExists(int id)
+        {
+          return (_context.Articulos?.Any(e => e.ArticuloId == id)).GetValueOrDefault();
+        }
+    }
+}
