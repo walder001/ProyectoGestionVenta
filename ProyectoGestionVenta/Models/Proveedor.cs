985d95717a3643a8f9d7db98853e4caea50fab5c
Ismarlyn Mendoza Hernández <Marlynm123@hotmail.com>
2023-10-14 12:12:28 -0400

    Modificado

--- ProyectoGestionVenta/Models/Proveedor.cs
+++ ProyectoGestionVenta/Models/Proveedor.cs
@@ -12,10 +12,14 @@ namespace ProyectoGestionVenta.Models
         }
 
         public int ProveedorId { get; set; }
+        [Phone(ErrorMessage = "Favor de ingresar correctamente el Rnc.")]
         public int? Rnc { get; set; }
         public string? Nombre { get; set; }
+        [Phone(ErrorMessage = "Favor de ingresar correctamente el Correo.")]
         public string Correo { get; set; } = null!;
+        [Phone(ErrorMessage = "Favor de ingresar correctamente el Representante.")]
         public string Representante { get; set; } = null!;
+        [Phone(ErrorMessage = "Favor de ingresar correctamente la Direccion")]
         public string Direccion { get; set; } = null!;
         [RegularExpression(@"^\d{3}-\d{3}-\d{4}$")]
         [Phone(ErrorMessage = "Favor de ingresar correctamente el numero Telefonico.")]
