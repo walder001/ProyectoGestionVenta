39aaaaacf3f7a2399ae296333d937481c569b785
walder001 <walderreyes34@gmail.com>
2023-10-21 10:12:18 -0400

    Implementacion de Jquery.Mask

--- ProyectoGestionVenta/wwwroot/js/site.js
+++ ProyectoGestionVenta/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    $('.telefono').mask('000-000-0000');
+});
